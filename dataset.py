9e7556f06e56f9a8c3aac83238123def824eff40
theFool32 <saber.rl32@gmail.com>
2021-04-26 11:11:33 +0800

    :recycle: Re-organize basic attack.

--- dataset.py
+++ dataset.py
@@ -1,59 +1,64 @@
 import os
-import csv
+import logging
 
 import numpy as np
 import torch
 from torchvision import transforms
 from PIL import Image
 
+
 class ImageNetDataset(torch.utils.data.Dataset):
-    def __init__(self, image_dir, label_dir, phase, total=1000, size=224):
-        assert phase in ['cln', 'adv']
-        self.image_dir = image_dir 
+    def __init__(self, image_dir, label_dir, phase, total=None, size=224):
+        assert phase in ["cln", "adv"]
+        self.image_dir = image_dir
         self.class_to_idx = np.load(label_dir, allow_pickle=True)[()]
         self.image_list = os.listdir(image_dir)
         self.image_list.sort()
         self.phase = phase
 
-        if phase == 'cln':
-            self.image_list = [item for item in self.image_list if 'png' in item]
-            self.transform = transforms.Compose([
-                transforms.Resize(int(size / 0.875)),
-                transforms.CenterCrop(size),
-                #transforms.Resize(size),
-                transforms.ToTensor()
-            ])
-        elif phase == 'adv':
-            self.image_list = [item for item in self.image_list if 'png' in item]
-            self.transform = transforms.Compose([
-                transforms.Resize(size),
-                transforms.ToTensor()
-            ])
-        assert len(self.image_list) == total
+        if phase == "cln":
+            self.image_list = [item for item in self.image_list if "png" in item]
+            self.transform = transforms.Compose(
+                [
+                    transforms.Resize(int(size / 0.875)),
+                    transforms.CenterCrop(size),
+                    # transforms.Resize(size),
+                    transforms.ToTensor(),
+                ]
+            )
+        elif phase == "adv":
+            self.image_list = [item for item in self.image_list if "png" in item]
+            self.transform = transforms.Compose(
+                [transforms.Resize(size), transforms.ToTensor()]
+            )
+        # assert len(self.image_list) == total
+        if total is None:
+            total = len(self.image_list)
 
+        if len(self.image_list) > total:
+            logging.warning(f"Attack {total} images with total: {len(self.image_list)}")
+            self.image_list = self.image_list[:total]
 
     def __getitem__(self, index):
         image_path = self.image_list[index]
-        with open(os.path.join(self.image_dir, image_path), 'rb') as f:
+        with open(os.path.join(self.image_dir, image_path), "rb") as f:
             with Image.open(f) as image:
-                image = image.convert('RGB')
+                image = image.convert("RGB")
         image = self.transform(image)
-        
+
         # if self.phase == 'cln':
         #     label = self.class_to_idx[self.image_list[index].split('_')[0]]
         # elif self.phase == 'adv':
         #     label = self.class_to_idx[index]
-        label = self.class_to_idx[self.image_list[index].split('_')[0]]
+        label = self.class_to_idx[self.image_list[index].split("_")[0]]
 
         return image, label, index
-
 
     def __len__(self):
         return len(self.image_list)
 
 
-
-def make_loader(image_dir, label_dir, phase, batch_size=1, total=1000, size=224):
+def make_loader(image_dir, label_dir, phase, batch_size=1, total=None, size=224):
     """
     Args:
         image_dir: image root directory
@@ -65,10 +70,7 @@ def make_loader(image_dir, label_dir, phase, batch_size=1, total=1000, size=224)
         list, dataloader
     """
     imgset = ImageNetDataset(image_dir, label_dir, phase, total=total, size=size)
-    loader = torch.utils.data.DataLoader(
-        imgset,
-        batch_size=batch_size
-    )
+    loader = torch.utils.data.DataLoader(imgset, batch_size=batch_size)
     return imgset.image_list, loader
 
 
@@ -82,4 +84,4 @@ def save_image(images, indexs, img_list, output_dir):
     for i, index in enumerate(indexs):
         cur_image = (images[i, :, :, :].transpose(1, 2, 0) * 255).astype(np.uint8)
         image = Image.fromarray(cur_image)
-        image.save(os.path.join(output_dir, img_list[index].split('.')[0] + '.png'))
+        image.save(os.path.join(output_dir, img_list[index].split(".")[0] + ".png"))
