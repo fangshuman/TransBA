e87c9b70343b081972fae0040aa292e0e2388b96
fangshuman <fsm@mail.com>
2021-12-04 16:24:51 +0800

    ⚡️  Add some source/target models.

--- models/__init__.py
+++ models/__init__.py
@@ -6,17 +6,23 @@ from . import cifar10_models  # for CIFAR-10
 
 # source model
 ImageNet_source_model = {
+    "vgg16": {"source_model": "vgg16_bn", "batch_size": 32},
     "vgg16_bn": {"source_model": "vgg16_bn", "batch_size": 32},
+    "vgg19": {"source_model": "vgg16_bn", "batch_size": 32},
+    "vgg19_bn": {"source_model": "vgg16_bn", "batch_size": 32},
     "resnet50": {"source_model": "resnet50", "batch_size": 80},
     "resnet152": {"source_model": "resnet152", "batch_size": 50},
     "densenet121": {"source_model": "densenet121", "batch_size": 32},
     "densenet201": {"source_model": "densenet201", "batch_size": 32},
     "inceptionv3": {"source_model": "inceptionv3", "batch_size": 64},
     "inceptionv4": {"source_model": "inceptionv4", "batch_size": 32},
-    "inceptionresnetv2": {"source_model": "inceptionresnetv2", "batch_size": 32},
+    "inceptionresnetv2": {"source_model": "inceptionresnetv2", "batch_size": 16},
 }
 ImageNet_target_model = {
+    "vgg16": 100,
     "vgg16_bn": 100,
+    "vgg19": 100,
+    "vgg19_bn": 100,
     "resnet50": 250,
     "resnet101": 250,
     "resnet152": 250,
